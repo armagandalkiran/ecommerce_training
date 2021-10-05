82f70dcf0f3b6261314d825e09db35844a806f83
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-10-06 00:18:43 +0300

    first commit !

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://static.bershka.net/4/photos2/2021/I/1/1/p/1023/860/001/1023860001_1_3_3.jpg?t=1633009271064",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://static.bershka.net/4/photos2/2021/I/1/1/p/1023/860/001/1023860001_1_3_3.jpg?t=1633009271064",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://static.bershka.net/4/photos2/2021/I/1/1/p/1023/860/001/1023860001_1_3_3.jpg?t=1633009271064",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
